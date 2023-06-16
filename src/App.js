4100ab1092fa31e46f9f7cd818cfd2f6768c0a4e
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 11:19:46 +0530

    basic hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//JSX = JS + HTML
+//If a function return html, this becomes a user defined tag
+//<App></App>
+//<App/>
+//compare with predefined tag
+//h1, p, li
+//why master case convention
+//for pre defoned tag :: lowercase :: <h1>
+//for useer define :: mastercase:: <App>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return <h1>Hello world</h1>;
+}
+
+//JSX = JS + HTML/XML
+//IF YOUR FUNCTION RETURN HTML
+//CONVENTION :: START YOUR FUNCTION NAME IN CAPITAL LETTER
+function Sum(){
+  return <h1>100</h1>;
 }
 
 export default App;
