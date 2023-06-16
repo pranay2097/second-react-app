cfd55490737a98aa5c18bac6a6f20c4aef57b288
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 14:29:19 +0530

    external styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 //HTML+JS=JSx
 //putting the brain in html
 //Styling :: color
-//
+//external styling
+import "./App.css";
+
+/**
+ * because 'class' is a keyword in js
+ */
 function App() {
-  //object literal
-  let style = {background: "red", color: "white"};
   return (
     <>
-      <h1 style={style}>Hello World</h1>
-      {/**first curly brace represent js expression */}
-      {/**second curly brace, its object literal */}
-      <h1 style={{background: "blue", color: "yellow"}}>hello world</h1>
-
-      {/**observation */}
-      {/**style attributes becomes camelcase in nature */}
-      {/**values are writing in double quotes */}
-      {/**multiple properties are separated by comma */}
-      <h1 style={{ backgroundColor: "green", color: "pink", padding: "8px"}}>Hello world</h1>
+      <h1 className="beautiful">Hello world</h1>
+      <h1 style={{color: "red"}}>hello</h1>
     </>
   );
 }
