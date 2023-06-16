26d155fe7f4803e34fafbd03a7d9518f53c9bf6d
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 12:02:23 +0530

    conditional return

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 //JSX SYNTAX
 function App(){
   //jsx single line :: with optional parenthesis
-    let jsx1 = <h1>Hello universe</h1>;
+    // let jsx1 = <h1>Good morning</h1>;
 
-    return jsx1;
+    // let jsx2 = (
+    //   <>
+    //     <h1>Good night</h1>
+    //   </>
+    // );
+
+    let jsx1 = <h1>log in </h1>;
+    let jsx2 = <h1>log out</h1>;
+
+    return true ? jsx1 : jsx2;
+    // return false ? jsx1 : jsx2;
 }
 
 
