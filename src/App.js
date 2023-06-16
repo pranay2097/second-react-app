c52c13c0f5c8d2eec496830bad8e8f651897f63c
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 15:11:01 +0530

    simple website

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="bg-primary text-light p-3">My Project</h1>
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, officia quaerat sunt porro laudantium cupiditate sit id accusantium facilis ipsa accusamus perferendis asperiores temporibus natus a eaque? Veniam, eaque consequatur.
+      </div>
 
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, officia quaerat sunt porro laudantium cupiditate sit id accusantium facilis ipsa accusamus perferendis asperiores temporibus natus a eaque? Veniam, eaque consequatur.
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, officia quaerat sunt porro laudantium cupiditate sit id accusantium facilis ipsa accusamus perferendis asperiores temporibus natus a eaque? Veniam, eaque consequatur.
+      </div>
     </>
   );
 }
