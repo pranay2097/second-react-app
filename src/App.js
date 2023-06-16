1f5ade3faf6d895b005133029026e72de5d9ce99
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 12:21:17 +0530

    formatting and auto suggesting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-//JSX SYNTAX
-function App(){
-  //jsx single line :: with optional parenthesis
-    // let jsx1 = <h1>Good morning</h1>;
-
-    // let jsx2 = (
-    //   <>
-    //     <h1>Good night</h1>
-    //   </>
-    // );
-
-    let jsx1 = <h1>log in </h1>;
-    let jsx2 = <h1>log out</h1>;
-
-    return true ? jsx1 : jsx2;
-    // return false ? jsx1 : jsx2;
+//is this working in your system
+//Auto suggestion feature
+//Auto formatting prettier
+function App() {
+  return (
+    <>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet illo ipsa inventore assumenda error harum est repellendus fugiat, placeat nemo?
+      </h1>
+    </>
+  );
 }
-
 
 export default App;
