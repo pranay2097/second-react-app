4bb4c57fa155bce00f902202f03cfc44c4eeafa5
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 15:18:52 +0530

    reusable block

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-
 function App() {
   return (
     <>
       <h1 className="bg-primary text-light p-3">My Project</h1>
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, officia quaerat sunt porro laudantium cupiditate sit id accusantium facilis ipsa accusamus perferendis asperiores temporibus natus a eaque? Veniam, eaque consequatur.
-      </div>
 
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, officia quaerat sunt porro laudantium cupiditate sit id accusantium facilis ipsa accusamus perferendis asperiores temporibus natus a eaque? Veniam, eaque consequatur.
-      </div>
+      <DataBlock/>
+      <DataBlock/>
+      <DataBlock/>
+      <DataBlock/>
+      <DataBlock/>
 
+      
+    </>
+  );
+}
+function DataBlock() {
+  return (
+    <>
       <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, officia quaerat sunt porro laudantium cupiditate sit id accusantium facilis ipsa accusamus perferendis asperiores temporibus natus a eaque? Veniam, eaque consequatur.
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, earum?</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta aliquid placeat incidunt esse ipsum provident iure nihil quam numquam ab, dignissimos laboriosam saepe ipsa architecto harum quidem ad! Quos, velit placeat. Accusamus sint unde odit nostrum similique rerum expedita eaque consequatur error fuga enim dolore iusto sapiente, assumenda ipsa aliquid quo quod libero in soluta eos alias autem labore provident. Nisi atque dolore, commodi reiciendis ab, nobis eaque dignissimos saepe quo veniam magnam aut obcaecati impedit provident corrupti nostrum deleniti porro ea explicabo tempore aliquid harum? Tenetur debitis harum, maxime exercitationem pariatur quaerat corrupti maiores quo recusandae voluptatem ipsum adipisci.
       </div>
     </>
   );
