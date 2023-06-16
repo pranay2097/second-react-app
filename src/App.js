09682b4c0ae0933c049977b735a099bfffbe1dbd
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 15:23:38 +0530

    static website reusable

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import DataBlock from "./DataBlock";
 function App() {
   return (
     <>
@@ -13,15 +14,6 @@ function App() {
     </>
   );
 }
-function DataBlock() {
-  return (
-    <>
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, earum?</h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta aliquid placeat incidunt esse ipsum provident iure nihil quam numquam ab, dignissimos laboriosam saepe ipsa architecto harum quidem ad! Quos, velit placeat. Accusamus sint unde odit nostrum similique rerum expedita eaque consequatur error fuga enim dolore iusto sapiente, assumenda ipsa aliquid quo quod libero in soluta eos alias autem labore provident. Nisi atque dolore, commodi reiciendis ab, nobis eaque dignissimos saepe quo veniam magnam aut obcaecati impedit provident corrupti nostrum deleniti porro ea explicabo tempore aliquid harum? Tenetur debitis harum, maxime exercitationem pariatur quaerat corrupti maiores quo recusandae voluptatem ipsum adipisci.
-      </div>
-    </>
-  );
-}
+
 
 export default App;
