4eef17deb244c3516e333cd4ab569524e52fbb1c
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-16 12:44:51 +0530

    basic interpolation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-//is this working in your system
-//Auto suggestion feature
-//Auto formatting prettier
-function App() {
-  return (
-    <>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet illo ipsa inventore assumenda error harum est repellendus fugiat, placeat nemo?
-      </h1>
-    </>
-  );
-}
+//mixing js in the html
+//mixing js in jsx
+//data binding in html
+//interpolation :: data binding in jsx
+function App(){
+  let str = `hello universe`;
 
+  //JSX :: view :: user interface
+  //mix html+js
+    return (
+      <>
+      <h1>Hello world</h1>
+      <h1>str</h1>
+      <h1>{str}</h1>
+      <h1>{str.toUpperCase()}</h1>
+
+      {[1, 1, 1].map((item) => (
+      <h1>Hello </h1>
+      ))}
+      
+      </>
+    );
+}
 export default App;
